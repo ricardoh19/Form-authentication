586a9c30e5045b341d8bcfb2ef4e04d9e06db38d
Ricardo Hernandez <hs.ricardoj@gmail.com>
2020-12-23 19:52:53 -0500

    Changed font

--- app.js
+++ app.js
@@ -7,20 +7,25 @@ submit.addEventListener('click',(event)=>{
     let password_form = document.getElementById("password").value;
     let retype_password_form = document.getElementById("retype_password").value;
     let feedback = document.getElementById("feedback");
+    let feedback_password = document.getElementById("feedback_password");
+    
 
     if (username_form.length >= 5){
         console.log("correct");
         feedback.textContent = "";
+        feedback_password.textContent = "";
     }
     else if (username_form.length < 5){
         console.log("False");
         feedback.textContent = "Username too short. Atleast 5 characters.";
+        feedback_password.textContent = "";
     }
     if (password_form.length != 0 && password_form == retype_password_form){
         console.log("Passwords match");
+
     }else if(password_form.length == 0 || password_form != retype_password_form){
         console.log("No password given.")
-        feedback.textContent += "No password given or passwords don't match.";
+        feedback_password.textContent = "No password given or passwords don't match.";
 
     }if(username_form.length >= 5 && password_form.length != 0 && password_form == retype_password_form){
         alert("Registration successful");
